dcd1041ee6aa2b3d93cf03d8f3cf629098fc0995
martinpanov <martin.panov98@gmail.com>
2023-09-10 23:16:36 +0300

    edited the galleries CSS and put lighbox into its own component

--- app/gallery/Gallery.tsx
+++ app/gallery/Gallery.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faX, faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import Lightbox from "./Lightbox";
 
 export default function Gallery() {
     const images = ['barber-giving-haircut', 'barber-adds-final-touches-to-a-haircut',
@@ -12,52 +11,22 @@ export default function Gallery() {
         'haircut2', 'haircut1', 'man-with-beard-getting-a-trim', 'haircut3', 'haircut5'];
 
     const [width, setWidth] = useState(0);
-    const [selectedImage, setSelectedImage] = useState('');
+    const [selectedImage, setSelectedImage] = useState<string>('');
+
     const breakpointMidScreen = 768;
     const breakpointLargeScreen = 1024;
 
     const handleWindowResize = () => setWidth(window.innerWidth);
 
-
     useEffect(() => {
         handleWindowResize();
         window.addEventListener("resize", handleWindowResize);
-        window.addEventListener("keydown", handleKeyDown);
 
-        // Return a function from the effect that removes the event listener
         return () => {
             window.removeEventListener("resize", handleWindowResize);
-            window.removeEventListener("keydown", handleKeyDown);
         };
     }, []);
 
-
-    const handlePreviousImage = () => {
-        setSelectedImage((prevSelectedImage) => {
-            const prevSelectedImageIndex = images.indexOf(prevSelectedImage);
-            if (prevSelectedImageIndex - 1 < 0) return prevSelectedImage;
-            return images[prevSelectedImageIndex - 1];
-        });
-    };
-
-    const handleNextImage = () => {
-        setSelectedImage((prevSelectedImage) => {
-            const prevSelectedImageIndex = images.indexOf(prevSelectedImage);
-            if (prevSelectedImageIndex === images.length - 1) return prevSelectedImage;
-            return images[prevSelectedImageIndex + 1];
-        });
-    };
-
-    const handleKeyDown = (event: KeyboardEvent) => {
-        console.log(event.key);
-        if (event.key === "ArrowRight") {
-            handleNextImage();
-        } else if (event.key === "ArrowLeft") {
-            handlePreviousImage();
-        } else if (event.key === "Escape") {
-            setSelectedImage('');
-        }
-    };
 
     return (
         <section className="flex justify-center px-10 pt-20 pb-10 xl:px-0">
@@ -94,34 +63,7 @@ export default function Gallery() {
                     </>
                 }
 
-                {selectedImage &&
-                    <div className="fixed inset-0 top-0 left-0 z-50 flex flex-col items-center w-full bg-black bg-opacity-90">
-                        <div className="flex items-center justify-between w-full p-6">
-                            <span className="flex-1 ml-10 text-2xl text-center md:ml-8">
-                                {images.indexOf(selectedImage) + 1}/{images.length}
-                            </span>
-                            <FontAwesomeIcon
-                                icon={faX}
-                                className="ml-auto text-3xl cursor-pointer"
-                                onClick={() => setSelectedImage('')}
-                            />
-                        </div>
-
-                        <div className="flex items-center justify-center w-full h-full gap-6 lg:gap-52 xl:gap-80">
-                            <FontAwesomeIcon
-                                icon={faArrowLeft}
-                                className="text-4xl cursor-pointer"
-                                onClick={handlePreviousImage}
-                            />
-                            <Image quality={100} src={`/${selectedImage}.jpg`} alt={selectedImage} width={1280} height={1920} className="object-contain h-full md:w-full w-60 sm:w-96 lg:max-w-lg md:max-w-md" />
-                            <FontAwesomeIcon
-                                icon={faArrowRight}
-                                className="text-4xl cursor-pointer"
-                                onClick={handleNextImage}
-                            />
-                        </div>
-                    </div>
-                }
+                <Lightbox images={images} selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
             </div>
         </section>
     );
---
+++ app/gallery/Lightbox.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX, faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import Image from "next/image";
+
+interface LightboxProps {
+    images: string[];
+    selectedImage: string;
+    setSelectedImage: Dispatch<SetStateAction<string>>;
+}
+
+
+export default function Lightbox({ images, selectedImage, setSelectedImage }: LightboxProps) {
+    const [touchStart, setTouchStart] = useState<number | null>(null);
+    const [touchEnd, setTouchEnd] = useState<number | null>(null);
+
+    const minSwipeDistance = 50;
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, []);
+
+    const handlePreviousImage = () => {
+        setSelectedImage((prevSelectedImage) => {
+            const prevSelectedImageIndex = images.indexOf(prevSelectedImage);
+            if (prevSelectedImageIndex - 1 < 0) return prevSelectedImage;
+            return images[prevSelectedImageIndex - 1];
+        });
+    };
+
+    const handleNextImage = () => {
+        setSelectedImage((prevSelectedImage) => {
+            const prevSelectedImageIndex = images.indexOf(prevSelectedImage);
+            if (prevSelectedImageIndex === images.length - 1) return prevSelectedImage;
+            return images[prevSelectedImageIndex + 1];
+        });
+    };
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+        console.log(event.key);
+        if (event.key === "ArrowRight") {
+            handleNextImage();
+        } else if (event.key === "ArrowLeft") {
+            handlePreviousImage();
+        } else if (event.key === "Escape") {
+            setSelectedImage('');
+        }
+    };
+
+    const onTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+        setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+        setTouchStart(e.targetTouches[0].clientX);
+    };
+
+    const onTouchMove = (e: React.TouchEvent<HTMLDivElement>) => setTouchEnd(e.targetTouches[0].clientX);
+
+    const onTouchEnd = () => {
+        if (!touchStart || !touchEnd) return;
+
+        const distance = touchStart - touchEnd;
+        const isLeftSwipe = distance > minSwipeDistance;
+        const isRightSwipe = distance < -minSwipeDistance;
+
+        if (isLeftSwipe) handleNextImage();
+        if (isRightSwipe) handlePreviousImage();
+    };
+
+    return (
+        <>
+            {selectedImage &&
+                <div className="fixed inset-0 top-0 left-0 z-50 flex flex-col items-center w-full bg-black bg-opacity-90">
+                    <div className="flex items-center justify-between w-full p-6">
+                        <span className="flex-1 ml-10 text-2xl text-center md:ml-8">
+                            {images.indexOf(selectedImage) + 1}/{images.length}
+                        </span>
+                        <FontAwesomeIcon
+                            icon={faX}
+                            className="ml-auto text-3xl cursor-pointer"
+                            onClick={() => setSelectedImage('')}
+                        />
+                    </div>
+
+                    <div
+                        className="flex items-center justify-center w-full h-full gap-6 lg:gap-52 xl:gap-80"
+                        draggable
+                        onTouchStart={onTouchStart}
+                        onTouchMove={onTouchMove}
+                        onTouchEnd={onTouchEnd}
+                    >
+                        <FontAwesomeIcon
+                            icon={faArrowLeft}
+                            className="hidden lg:block lg:absolute lg:top-[55%] lg:-translate-y-2/4 lg:-translate-x-2/4 lg:text-white lg:left-[10%] lg:text-4xl lg:cursor-pointer"
+                            onClick={handlePreviousImage}
+                        />
+                        <Image quality={100} src={`/${selectedImage}.jpg`} alt={selectedImage} width={1280} height={1920} className="object-contain h-full md:w-full w-60 sm:w-96 lg:max-w-lg md:max-w-md" />
+                        <FontAwesomeIcon
+                            icon={faArrowRight}
+                            className="hidden lg:block lg:absolute lg:top-[55%] lg:-translate-y-2/4 lg:-translate-x-2/4 lg:text-white lg:right-[8%] lg:text-4xl lg:cursor-pointer"
+                            onClick={handleNextImage}
+                        />
+                    </div>
+                </div>
+            }
+        </>
+    );
+}
